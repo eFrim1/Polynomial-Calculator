859efef90f0f601131573ccfe11686c65d7fd25c
eFrim1 <eleazarfrim@gmail.com>
2024-03-04 12:46:39 +0200

    initial commit

---
+++ src/main/java/com/example/pt2024_30227_frim_eleazar_assignment_1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.pt2024_30227_frim_eleazar_assignment_1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.pt2024_30227_frim_eleazar_assignment_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.pt2024_30227_frim_eleazar_assignment_1 to javafx.fxml;
+    exports com.example.pt2024_30227_frim_eleazar_assignment_1;
+}
